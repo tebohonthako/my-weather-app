4b7185af3ff67f2badc45d6d4bccbf3c13aca6d2
T E B O O <110821589+tebohonthako@users.noreply.github.com>
2023-04-16 13:51:47 +0200

    Add files via upload

---
+++ my-weather-app/index.js
@@ -0,0 +1,9 @@
+// FEATURE 1
+
+function city(event) {
+  event.preventDefault();
+  let dateAndTime = document.querySelector("#search-event");
+  dateAndTime.addEventListener("click", search - event.value);
+}
+let dateAndTime = document.querySelector("h1");
+dateAndTime.addEventListener("click", city);
